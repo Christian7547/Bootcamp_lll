ee9f6b9d5be71b1145feeac05da28530f92419fc
ClownBaal <ivanmendezchambi@gmail.com>
2023-06-04 13:05:52 -0400

    second push

--- Bootcamp_lll/MainWindow.xaml.cs
+++ Bootcamp_lll/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Bootcamp_lll
 {
@@ -9,5 +10,54 @@ namespace Bootcamp_lll
         {
             InitializeComponent();
         }
+
+        #region Effect to Window
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        #endregion
+
+        #region Buttones ->  Minimize - Maximize - Close
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #region Views <> Pages
+        private void rdHome_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new System.Uri("Views/HomePage.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void rdUsers_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new System.Uri("Views/UsersPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void rdListUsers_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new System.Uri("Views/ListUsersPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void rdOptions_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new System.Uri("Views/OptionsPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+        #endregion
     }
 }
